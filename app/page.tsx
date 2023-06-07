e89cef43a7e673ead19baf462b6866f3009bd12b
Sagi Mines <sagi1236@gmail.com>
2023-06-07 15:30:21 +0300

    Added to the home page the text

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,16 @@
-export default function Home() {
-  return <main>Main</main>;
-}
+const Home = () => {
+  return (
+    <section className="w-full flex-center flex-col">
+      <h1 className="head-text text-center">
+        Discover & Share
+        <br className="max-md:hidden" />
+        <span className="orange-gradient text-center"> AI-Powered Prompts</span>
+      </h1>
+      <p className="desc text-center">
+        Promptactics is an open-source AI prompting tool for modern world to
+        discover, create and share creative prompts
+      </p>
+    </section>
+  );
+};
+export default Home;
